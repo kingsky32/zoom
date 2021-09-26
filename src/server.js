868b7f092aee18305fdc074043994ed0c727222f
Seung Ju <kingsky32@gmail.com>
2021-09-26 21:40:26 +0900

    Frontend Setup

--- src/server.js
+++ src/server.js
@@ -7,8 +7,8 @@ const app = express();
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
-
 app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
 
 app.listen(PORT, () => {
   console.log(`✅ Listening on http://localhost:${PORT}`);
